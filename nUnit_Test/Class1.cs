08caf68c4f0724b4735087b3a9272f7655bc6ac2
Jatin Verma <jverma4006@conestogac.on.ca>
2024-02-12 19:17:40 -0500

    Add project files.

---
+++ nUnit_Test/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using NUnit_JatinVerma;
+
+namespace nUnit_Test_JatinVerma
+{
+    public class Class1
+    {
+
+        [Test]
+
+        public void TriangleTestOutput()
+        {
+            //arrange
+            int firstAngle = 60;
+            int secondAngle =60;
+            int thirdAngle = 60;
+
+            string expected = "The triangle is valid.";
+
+            //act
+
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+
+            //assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
